75a40b74c5a738c214aaa421906d00f943e400a4
yosuke aga <yosuke0607@aga-no-macbook-pro.local>
2016-03-05 17:15:12 +0900

    課題

---
+++ Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  
+//
+//  Created by aga yosuke on 2016/03/05.
+//
+//
+
+import UIKit
+
+class Task: Object {
+
+}
---
+++ taskapp2/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  
+//
+//  Created by aga yosuke on 2016/03/05.
+//
+//
+
+import RealmSwift
+
+
+class Task: Object {
+
+}
